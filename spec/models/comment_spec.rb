f384af5a73bbe0e1c3f77546a3bf498395d3504e
Sergey Boyko <badwolf6661@icloud.com>
2021-10-17 19:48:28 +0300

    Add test for comment validation length

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 describe Comment do
-  it { should belong_to :article }
+  describe 'validation' do
+    it { should belong_to :article }
+  end
+  describe 'length validation' do
+    it { should validate_length_of(:body).is_at_most(4000) }
+  end
+
 
 end
