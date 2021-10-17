91c059241a42f17f9fb3324f67100f9f08795d4c
Sergey Boyko <badwolf6661@icloud.com>
2021-10-17 21:13:30 +0300

    Create test for user registration

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
 
 feature 'Contact Creation' do
-  scenario 'allows aces to contacts page' do
+  scenario 'allows acces to contacts page' do
     visit '/contacts'
     expect(page).to have_content 'Напишите нам!'
+  end
+  scenario 'allows a guest to create contact' do
+    visit '/contacts'
+    fill_in :contact_email, :with => 'user@example.com'
+    fill_in :contact_message, :with => 'something'
+    click_button 'Отправить письмо'
+    expect(page).to have_content 'успешно'
   end
 end
